3b83ea2344c4c8d0c6320456a1bfdd6681a1bf99
RAHUL KHALLAR <khallarrahul@gmail.com>
2023-08-25 12:53:47 -0700

    changes imports

--- seeds.py
+++ seeds.py
@@ -1,7 +1,7 @@
 from models import User, Contact, Message
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from datetime import datetime
+
 
 print("Seeding DB....")
 
